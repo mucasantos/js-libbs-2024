378d8819b0be8e3453a2c4526842ff781f6f18be
Samuel Luis dos Santos <mucasantos@gmail.com>
2024-04-08 16:04:44 -0300

    Aula07 - fetch - automatizar o processo

--- aula-07/script.js
+++ aula-07/script.js
@@ -10,12 +10,38 @@ async function getPets() {
 }
 */
 
-fetch("https://rickandmortyapi.com/api/character/?page=1")
-    .then(rest => rest.json())
+const urlRick = "https://rickandmortyapi.com/api/character/?page=1";
+const main = document.getElementById("container");
+
+fetch(urlRick, )
+    .then(rest => rest.json() )
     .then(data => cards(data))
 
 function cards(data) {
 
+    const dataArray = data.results;
     //Criar os cards de forma automática
+
+    dataArray.forEach(element => {
+        const card = document.createElement("div");
+        card.className = "card"
+        card.innerHTML = `
+        <img src=${element.image} alt=${element.name}>
+        <div class="names">
+            <h3>Nome:</h3>
+            <h3>${element.name}</h3>
+        </div>
+        <div class="specie">
+            <h3>Espécie:</h3>
+            <h3>${element.species}</h3>
+        </div>
+        <div class="status">
+            <h3>Status:</h3>
+            <h3>${element.status}</h3>
+        </div>
+        `
+        
+        main.appendChild(card)
+    });
 
 }
